59c77a50bd95f8706267054573426b41b9a44fb6
Argjend <argjendxhigoli@gmail.com>
2016-04-04 10:01:06 +0200

    Modified Search for directions

--- app/models/business.rb
+++ app/models/business.rb
@@ -18,9 +18,9 @@ class Business < ActiveRecord::Base
   validates :name, presence: true
   # validates :address, presence: true
 
-  geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
-  
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+
   after_create  :create_dir
   after_destroy :destroy_dir
 
